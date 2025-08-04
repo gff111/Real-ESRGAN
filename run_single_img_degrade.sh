cc7cbf0053bb07ff0cc7145ec427346c92828f2b
yinnhao <>
2025-08-04 21:34:57 +0800

    adaddadd meta gen

--- run_single_img_degrade.sh
+++ run_single_img_degrade.sh
@@ -3,4 +3,4 @@ python degrade_single_image.py \
     --input /root/paddlejob/workspace/env_run/zhuyinghao/datasets/dataset_lite/aigc_ocr_part1_img_multiscale_sub/cndroop_location_d1a5885efa3d35a9f634575e9a0cab1cT1_s003.png \
     --output ./degrade.png \
     --device cuda \
-    --seed 42
+    --seed 1

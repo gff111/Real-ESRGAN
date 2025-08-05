41ef80b09e7077b9dc0848e24fad506a33d6783e
yinnhao <>
2025-08-05 17:26:46 +0800

    add train jpeg2

--- infer_bd.sh
+++ infer_bd.sh
@@ -9,9 +9,27 @@
 # python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/qiucan40_${item}G --model_path $ori_weight --tile 512
 # done
 
-checkpoint=("5000" "10000" "20000" "50000" "60000" "70000" "80000" "90000")
+# checkpoint=("5000" "10000" "20000" "50000" "60000" "70000" "80000" "90000")
+# for item in "${checkpoint[@]}"
+# do
+# ori_weight=/root/paddlejob/workspace/env_run/zhuyinghao/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_yh/models/net_g_${item}.pth
+# python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/yinghao10/yinghao10_${item}G --model_path $ori_weight --tile 512
+# done
+
+
+# yinghao20
+checkpoint=("5000" "10000" "20000" "25000")
 for item in "${checkpoint[@]}"
 do
-ori_weight=/root/paddlejob/workspace/env_run/zhuyinghao/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_yh/models/net_g_${item}.pth
-python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/yinghao10/yinghao10_${item}G --model_path $ori_weight --tile 512
+ori_weight=/root/paddlejob/workspace/env_run/zhuyinghao/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_yh_no_usm/models/net_g_${item}.pth
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/yinghao20/yinghao20_${item}G --model_path $ori_weight --tile 512
+done
+
+# yinghao30
+
+checkpoint=("5000" "10000" "20000")
+for item in "${checkpoint[@]}"
+do
+ori_weight=/root/paddlejob/workspace/env_run/zhuyinghao/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_yh_30_online_degrade/models/net_g_${item}.pth
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/yinghao30/yinghao30_${item}G --model_path $ori_weight --tile 512
 done

95d49a05880d3fcccf2eb0d5fe7f8741e0e33a4f
yinnhao <>
2025-08-05 12:56:38 +0800

    update train

--- infer_bd.sh
+++ infer_bd.sh
@@ -9,9 +9,9 @@
 # python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/qiucan40_${item}G --model_path $ori_weight --tile 512
 # done
 
-checkpoint=("5000" "10000" "20000" "50000")
+checkpoint=("5000" "10000" "20000" "50000" "60000" "70000" "80000" "90000")
 for item in "${checkpoint[@]}"
 do
-ori_weight=/root/paddlejob/workspace/qiucan/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_400k_qc_text2/models/net_g_${item}.pth
-python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/qiucan30/qiucan30_${item}G --model_path $ori_weight --tile 512
+ori_weight=/root/paddlejob/workspace/env_run/zhuyinghao/Real-ESRGAN/experiments/finetune_RealESRGANx4plus_yh/models/net_g_${item}.pth
+python inference_realesrgan.py -n RealESRGAN_x4plus -i inputs/test_set_sr --output ./results/yinghao10/yinghao10_${item}G --model_path $ori_weight --tile 512
 done

1215912b41a72819fea13683a04d9ac389ef43ac
yinnhao <>
2025-08-05 11:12:25 +0800

    run train

--- realesrgan/train.py
+++ realesrgan/train.py
@@ -6,6 +6,16 @@ import realesrgan.archs
 import realesrgan.data
 import realesrgan.models
 
+import sys
+
+# 将 --local-rank 转换为 --local_rank
+for i, arg in enumerate(sys.argv):
+    if arg.startswith('--local-rank='):
+        value = arg.split('=')[1]
+        sys.argv[i] = f'--local_rank={value}'
+    elif arg == '--local-rank' and i+1 < len(sys.argv):
+        sys.argv[i] = '--local_rank'
+
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
     train_pipeline(root_path)
--- run_single_img_degrade.sh
+++ run_single_img_degrade.sh
@@ -1,6 +1,5 @@
-python degrade_single_image.py \
-    --config options/finetune_realesrgan_x4plus.yml \
+python new_degrade_simgle_img.py \
+    --config options/finetune_realesrgan_x4plus_yh.yml \
     --input /root/paddlejob/workspace/env_run/zhuyinghao/datasets/dataset_lite/aigc_ocr_part1_img_multiscale_sub/cndroop_location_d1a5885efa3d35a9f634575e9a0cab1cT1_s003.png \
-    --output ./degrade.png \
+    --output ./degrade1.png \
     --device cuda \
-    --seed 1
